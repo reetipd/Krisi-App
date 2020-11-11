340e6863d590926f346b63d6a787b0584a1b4cef
Laxmi Chapagain <laxmichapagain1996@gmail.com>
2020-11-11 16:28:57 +0545

    Formatting

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const router = express.Router();
 const Products = require('../models/products')
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   let product = await Products.find();
-  res.render('index',{productList:product});
+  res.render('index', { productList: product });
 });
 
 module.exports = router;
